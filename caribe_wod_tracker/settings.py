5bfdd9abdbc0841326111c11b31fdd8141e1c555
sectic-desarrolador4 <sectic-desarrolador4@meta.gov.co>
2025-05-16 16:09:35 -0500

    Primer commit

--- caribe_wod_tracker/settings.py
+++ caribe_wod_tracker/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-oeeimb@9x=7+a)=bg5rlho=43=^y612f+_b4bmkq5!ao2xks%+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
